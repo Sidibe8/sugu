ee43b75dd55e9cc2e3123d592bf45e398d36d304
Sidibe8 <syoro4663@gmail.com>
2024-11-24 09:23:00 +0100

    com

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log l’erreur pour debug
+  res.status(500).json({ message: "Internal Server Error", error: err.message });
+});
+
+
 // Définissez les origines autorisées
 const allowedOrigins = [
   "http://localhost:3000",
@@ -40,7 +46,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
