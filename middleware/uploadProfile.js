013f654546228222499630870ac3e116b726bd7e
Sidibe8 <syoro4663@gmail.com>
2024-11-24 10:29:24 +0100

    histoire de dossier

--- middleware/uploadProfile.js
+++ middleware/uploadProfile.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/profile/"); // Dossier pour les images de profil
+    cb(null, "uploads/profile"); // Dossier pour les images de profil
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname)); // Ajoute un timestamp au nom du fichier
